99e65b489c8b7694c33fa9cd1cd05e1652cb656d
Dik van Genuchten <h.j.m.v.genuchten@student.tue.nl>
2022-03-01 14:59:32 +0000

    Refactor model

--- code/Adversarial-Classification/src/model.py
+++ code/Adversarial-Classification/src/model.py
@@ -5,20 +5,24 @@ import torch
 class MnistModel(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 10, kernel_size=5),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+            nn.Conv2d(10, 20, kernel_size=5),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+            nn.Dropout2d(),
+            nn.Flatten(),
+            nn.Linear(320, 50),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(50, 10),
+            nn.LogSoftmax(),
+        )
 
     def forward(self, x):
-        x = nn.functional.relu(nn.functional.max_pool2d(self.conv1(x), 2))
-        x = nn.functional.relu(nn.functional.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = nn.functional.relu(self.fc1(x))
-        x = nn.functional.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return nn.functional.log_softmax(x)
+        return self.model(x)
 
     def inference(self, x):
         out = self.forward(x)
