7a7d1d02afa77ba3e562d8a26df90072bb42ab4d
Dik van Genuchten <h.j.m.v.genuchten@student.tue.nl>
2022-06-02 18:08:26 +0200

    Fix paths

--- code/ShowDistractAndDeceive/test/conftest.py
+++ code/ShowDistractAndDeceive/test/conftest.py
@@ -5,8 +5,8 @@ import torch
 
 import utils
 
-MODEL_PATH = "data/BEST_checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar"
-WORD_MAP_PATH = "data/WORDMAP_coco_5_cap_per_img_5_min_word_freq.json"
+MODEL_PATH = "/data/BEST_checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar"
+WORD_MAP_PATH = "/data/WORDMAP_coco_5_cap_per_img_5_min_word_freq.json"
 
 
 @pytest.fixture(params=["cuda", "cpu"], scope="module")
