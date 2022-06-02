4f223a2b3bdeaf7c6a444c235b01d2d0262050e5
Dik van Genuchten <h.j.m.v.genuchten@student.tue.nl>
2022-06-02 14:50:26 +0200

    Move folders

--- code/ShowDistractAndDeceive/src/main.py
+++ code/ShowDistractAndDeceive/src/main.py
@@ -169,13 +169,13 @@ if __name__ == "__main__":
     parser.add_argument(
         "--model-path",
         type=str,
-        default="data/BEST_checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar",
+        default="/data/BEST_checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar",
         help="Path to the show attend and tell model checkpoint.",
     )
     parser.add_argument(
         "--word-map",
         type=str,
-        default="data/WORDMAP_coco_5_cap_per_img_5_min_word_freq.json",
+        default="/data/WORDMAP_coco_5_cap_per_img_5_min_word_freq.json",
         help="Path to the json wordmap of the model.",
     )
     parser.add_argument(
